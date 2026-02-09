c2f2c531c275193b690e8dbe27b8bd81dc7d8a6e
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-02-09 14:18:57 +0100

    test: adds test project autogenerated by Uno

---
+++ Wordle.Uno.Tests/AppInfoTests.cs
@@ -0,0 +1,20 @@
+using Wordle.Model;
+
+namespace Wordle.Uno.Tests;
+
+public class AppInfoTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void AppInfoCreation()
+    {
+        var appInfo = new AppConfig { Environment = "Test" };
+
+        appInfo.Should().NotBeNull();
+        appInfo.Environment.Should().Be("Test");
+    }
+}
